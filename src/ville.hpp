f2cad831a95c33effac22370503d53e560ee4fe2
zebybron <92932601+zebybron@users.noreply.github.com>
2022-02-10 11:51:53 +0100

    Update ville.hpp

--- src/ville.hpp
+++ src/ville.hpp
@@ -1,1 +1,26 @@
+#ifndef VILLE_HPP
+#define VILLE_HPP
 #include <string>
+
+class Ville {
+    private:
+
+        std::string nom;
+        int nbVilleAccessible;
+        Ville** villeAccesible;
+
+    public:
+
+        Ville();
+        Ville(std::string nom);
+        Ville(std::string nom,int nbVille);
+
+        std::string getNom() const ;
+        int getNbVilleAccessible() const ;
+
+        ~Ville();
+};
+
+
+
+#endif
