2cfad599c32c130682c54f67ac345badac90012d
zebybron <92932601+zebybron@users.noreply.github.com>
2022-02-10 11:50:58 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
+#include <iostream>
 #include "ville.hpp"
+#include "reseau.hpp"
 
 using namespace std;
 
 int main(){
+    
+    Ville A{"Addis-Abeba",2};
+    Ville B{"Addis-Abeba",2};
+    Ville C{"Addis-Abeba",2};
+    Ville D{"Addis-Abeba",2};
 
-  return 1;
+    return 0;
 }
