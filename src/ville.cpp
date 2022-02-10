91b35613e9338aaf6238ab82c114a6ad01b9a2c1
zebybron <92932601+zebybron@users.noreply.github.com>
2022-02-10 11:51:32 +0100

    Update ville.cpp

--- src/ville.cpp
+++ src/ville.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
 #include "ville.hpp"
+
+using namespace std;
+
+Ville::Ville() : nom{"Inconnu"}, nbVilleAccessible{10}
+{
+    villeAccesible = new Ville*[nbVilleAccessible];
+}
+
+Ville::Ville(string nom) : nom{nom}, nbVilleAccessible{10}
+{
+    villeAccesible = new Ville*[nbVilleAccessible];
+}
+
+Ville::Ville(std::string nom,int nbVille) : nom{nom}, nbVilleAccessible{nbVille}
+{
+    villeAccesible = new Ville*[nbVilleAccessible];
+}
+
+string Ville::getNom() const{
+    return nom;
+}
+
+int Ville::getNbVilleAccessible() const{
+    return nbVilleAccessible;
+}
+
+Ville::~Ville(){
+
+    delete []villeAccesible;
+}
