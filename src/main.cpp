229be944624232d22fc7222a1d3a5773b102f874
zebybron <92932601+zebybron@users.noreply.github.com>
2022-02-28 11:12:45 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,33 @@ using namespace std;
 
 int main(){
     
-    Ville A{"Addis-Abeba",2};
-    Ville B{"Addis-Abeba",2};
-    Ville C{"Addis-Abeba",2};
-    Ville D{"Addis-Abeba",2};
+    Reseau reseau{"France"};
 
+    Ville addis{"Addis-Abeba",1};
+    Ville bogota{"Bogota",2};
+    Ville doha{"Doha",3};
+    Ville copenhague{"Copenhague",4};
+
+    reseau.initMatriceAccessible();
+
+    reseau.ajouterVille(&addis);
+    reseau.ajouterVille(&bogota);
+    reseau.ajouterVille(&doha);
+    reseau.ajouterVille(&copenhague);
+
+    addis.ajouterVilleAccessible(&bogota);
+    addis.ajouterVilleAccessible(&doha);
+    
+    bogota.ajouterVilleAccessible(&addis);
+    
+    doha.ajouterVilleAccessible(&bogota);
+    doha.ajouterVilleAccessible(&copenhague);
+
+    cout << addis << endl;
+    cout << bogota << endl;
+    cout << doha << endl;
+    cout << copenhague << endl;
+
+    reseau.test(addis,bogota);
     return 0;
 }
