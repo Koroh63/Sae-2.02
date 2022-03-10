d62e16828251c395d390d3f03bc035ab1fd7e606
zebybron <92932601+zebybron@users.noreply.github.com>
2022-03-10 16:32:44 +0100

    Update reseau.cpp

--- src/reseau.cpp
+++ src/reseau.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include "reseau.hpp"
 #include "ville.hpp"
 
@@ -40,3 +41,57 @@ void Reseau::initMatriceAccessible() {
                         {0,0,0,0}};
 }
 
+void Reseau::rechercheLargeur(Ville* villeStart,Ville* villeEnd){
+    
+    vector<Ville*> chemin ;
+    chemin.push_back(villeStart);
+
+    for ( Ville* tmpVille : villeStart->getVilleAccessible()){
+        chemin = rechercheRecur(chemin,tmpVille,villeEnd);
+    }
+
+    for(Ville* tmp : chemin){
+        cout << tmp->getNom() << " -- " ;
+    }
+    cout << endl; 
+}
+
+vector<Ville*> Reseau::rechercheRecur(vector<Ville*> chemin,Ville* villeStart,Ville* villeEnd) const {
+    chemin.push_back(villeStart);
+    for ( Ville* tmpVille : villeStart->getVilleAccessible()){
+        if( tmpVille == villeEnd ){
+            chemin.push_back(tmpVille);
+
+            return chemin ;
+        }
+    }
+    chemin.erase(chemin.end()-1);
+    return chemin;
+}
+
+void Reseau::rechercheDestination(Ville* villeStart){
+    vector<Ville*> villeAces ;
+
+    villeAces.push_back(villeStart);
+
+    villeAces = fonction(villeStart->getVilleAccessible(),villeAces,villeStart);
+
+    for(Ville* tmp : villeAces){
+        cout << tmp->getNom() << " | " ;
+    }
+    cout << endl;     
+}
+
+
+vector<Ville*> Reseau::fonction(vector<Ville*> villeDispo,std::vector<Ville*> villeAces,Ville* ville){
+
+    vector<Ville*>::iterator it;
+
+    for(Ville* tmp : villeDispo){
+        if ( find(villeAces.begin(),villeAces.end(),tmp) == villeAces.end()){
+            villeAces.push_back(tmp);
+            return fonction(tmp->getVilleAccessible(),villeAces,tmp);
+        }
+    }
+    return villeAces;
+}
